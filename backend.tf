08b28ad4f8d3b1fa402cad17b57d80aae0c90fef
vk9201 <41668273+vk9201@users.noreply.github.com>
2022-05-15 18:43:11 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,8 +3,6 @@ terraform {
     bucket         = "cloudlogic20-tfstate"
     key            = "terraformdemo.tfstate"
     region         = "us-east-1"
-    access_key = "AKIAV5QGOZ7ZUKYP5ZN3"
-    secret_key = "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"
     dynamodb_table = "s3-state-lock"
   }
 }
