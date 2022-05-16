078dc52d35f856c06528f40563a08ed9364f4c4d
vk9201 <41668273+vk9201@users.noreply.github.com>
2022-05-16 19:21:09 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "cloudlogic20-tfstate1"
-    key            = "terraformdemo.tfstate1"
-    region         = "us-east-1"
-    dynamodb_table = "s3-state-lock"
-  }
-}
